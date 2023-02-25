858451ac05337467f040075bf14c75490c1cbaf0
Sanjana221 <90644548+Sanjana221@users.noreply.github.com>
2023-02-25 13:54:51 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyD7AYwuzeSJ5G4PERMf-Xfr9_a09blZHS0";
+
+export default API_KEY;
---
+++ useGoogleSearch.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import API_KEY from "./keys";
+
+const CONTEXT_KEY = "d170e556bf1094cd4";
+
+const useGoogleSearch = (term) => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
+
+    fetchData();
+  }, [term]);
+
+  return { data };
+};
+export default useGoogleSearch;
